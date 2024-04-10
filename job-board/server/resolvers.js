83321a4daff1d6b608e61dc0e5403399109b480f
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2024-04-10 21:41:46 +0300

    custom error handling for case when no data was received from database

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,18 +1,42 @@
 import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
 import { getCompany } from "./db/companies.js";
+import { GraphQLError } from "graphql";
 
 export const resolvers = {
+  Query: {
+    jobs: () => getJobs(),
+    job: async (_, args) => {
+      const job = await getJob(args.id);
+      if (!job) {
+        onNotFoundError("No job found with id: " + args.id + ".");
+      }
+      return job;
+    },
+
+    company: async (_, args) => {
+      const company = await getCompany(args.id);
+
+      if (!company) {
+        onNotFoundError(`Company with id: ${args.id} not exist.`);
+      }
+
+      return company;
+    },
+  },
+
   Job: {
     date: (job) => job.createdAt,
     company: (job) => getCompany(job.companyId),
   },
+
   Company: {
     title: (company) => company.name,
     jobs: (company) => getJobsByCompany(company.id),
   },
-  Query: {
-    jobs: () => getJobs(),
-    job: (_, args) => getJob(args.id),
-    company: (_, args) => getCompany(args.id),
-  },
 };
+
+function onNotFoundError(message) {
+  throw new GraphQLError(message, {
+    extensions: { code: "NOT_FOUND" },
+  });
+}
