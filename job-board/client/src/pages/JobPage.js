304d9c1e32ce5f293ecd98e3dbb4d968b1fe3410
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2024-04-11 11:34:22 +0300

    client side one way to handle errors for JobPage.js

--- job-board/client/src/pages/JobPage.js
+++ job-board/client/src/pages/JobPage.js
@@ -1,20 +1,59 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { formatDate } from "../lib/formatters";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getAJob } from "../graphql/queries";
 
 function JobPage() {
   const { jobId } = useParams();
-  const [job, setJob] = useState(null);
+  const [state, setState] = useState({
+    isLoading: true,
+    job: null,
+    error: null,
+  });
 
-  useEffect(() => {
-    getAJob(jobId).then(setJob);
+  const fetchJob = useCallback(async () => {
+    try {
+      const job = await getAJob(jobId);
+      setState((prevState) => {
+        return {
+          ...prevState,
+          job,
+          isLoading: false,
+          error: null,
+        };
+      });
+    } catch (e) {
+      setState((prevState) => {
+        return {
+          ...prevState,
+          job: null,
+          isLoading: false,
+          error: e,
+        };
+      });
+    }
   }, [jobId]);
 
-  if (!job) {
+  useEffect(() => {
+    fetchJob();
+  }, [fetchJob]);
+
+  const { error, job, isLoading } = state;
+
+  if (isLoading) {
     return "Loading...";
   }
+
+  if (error) {
+    return (
+      <>
+        <h1>{error.message}</h1>
+        <p>{error.code}</p>
+      </>
+    );
+  }
+
   return (
     <div>
       <h1 className="title is-2">{job.title}</h1>
