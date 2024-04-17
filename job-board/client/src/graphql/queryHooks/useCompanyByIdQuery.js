1cb17c10595146f8ebc339e6cae29c9dfe4fc890
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2024-04-17 15:54:06 +0300

    migration to typescript

--- job-board/client/src/graphql/queryHooks/useCompanyByIdQuery.js
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@apollo/client";
-import { getACompanyQuery } from "../queries";
-
-export function useCompanyByIdQuery(companyId) {
-  return useQuery(getACompanyQuery, {
-    variables: { id: companyId },
-  });
-}
--- job-board/client/src/graphql/queryHooks/useJobByIdQuery.js
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@apollo/client";
-import { getSingleJobQuery } from "../queries";
-
-export const useJobBuIdQuery = (jobId) => {
-  return useQuery(getSingleJobQuery, {
-    variables: { id: jobId },
-  });
-};
